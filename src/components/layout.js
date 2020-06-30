0200feb7dd6bd009905a8e3a04624758cdaedfa7
Dave Lindberg <davelindb@gmail.com>
2020-06-30 18:45:08 -0400

    added navigation

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+import Menu from "./Menu"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -31,8 +32,10 @@ const Layout = ({ children }) => {
           margin: `0 auto`,
           maxWidth: 960,
           padding: `0 1.0875rem 1.45rem`,
+          paddingTop: 0,
         }}
       >
+        <Menu />
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
