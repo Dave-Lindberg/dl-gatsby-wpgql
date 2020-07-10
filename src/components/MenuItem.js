ad1b81a33e9f7591dbcceca77fc41d47326483c2
Dave Lindberg <davelindb@gmail.com>
2020-07-10 19:45:06 -0400

    ready for new Chakra-ui menu branch

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,10 +1,13 @@
 import React from "react"
 import { CreateLocalLink } from "../utils"
 import { Link } from "gatsby"
+import { Text }from "@chakra-ui/core"
 
 const MenuItem = ({ menuItem, wordPressUrl }) => {
   return (
-    <Link style={{marginRight: '20px' }} to={CreateLocalLink(menuItem, wordPressUrl)}>{menuItem.label}</Link>
+    <Text mt={{ base: 4, md: 0 }} mr={6} display="block" >
+      <Link  to={CreateLocalLink(menuItem, wordPressUrl)}>{menuItem.label}</Link>
+    </Text>
   )
 }
 
